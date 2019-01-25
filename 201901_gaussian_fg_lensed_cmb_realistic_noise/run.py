d4b438d55b5b16c262dfa6f63aa87431f65b1063
Andrea Zonca <code@andreazonca.com>
2019-01-25 13:24:52 -0800

    updated to support multiple channels

--- 201901_gaussian_fg_lensed_cmb_realistic_noise/run.py
+++ 201901_gaussian_fg_lensed_cmb_realistic_noise/run.py
@@ -11,7 +11,7 @@ import so_pysm_models
 simulator = mapsims.from_config(
     "example_config.cfg"
 )
-output_map = simulator.execute()
+output_map = simulator.execute(write_outputs=False)[simulator.channels[0]]
 
 expected_map = hp.read_map(
     data.get_pkg_data_filename("tests/data/example_run.fits.gz", package="mapsims"), (0, 1, 2)
