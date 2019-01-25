1fd116fa2243136dac33126bbb892f9b246b0abb
Andrea Zonca <code@andreazonca.com>
2019-01-24 19:46:51 -0800

    running mapsims nside 16 test case
    need to create a symlink `mapsims` that points to the inner package
    to get the data

---
+++ 201901_gaussian_fg_lensed_cmb_realistic_noise/run.py
@@ -0,0 +1,19 @@
+import numpy as np
+import healpy as hp
+
+from astropy.utils import data
+
+import mapsims
+import so_pysm_models
+
+
+
+simulator = mapsims.from_config(
+    "example_config.cfg"
+)
+output_map = simulator.execute()
+
+expected_map = hp.read_map(
+    data.get_pkg_data_filename("tests/data/example_run.fits.gz", package="mapsims"), (0, 1, 2)
+)
+np.testing.assert_allclose(output_map, expected_map)
