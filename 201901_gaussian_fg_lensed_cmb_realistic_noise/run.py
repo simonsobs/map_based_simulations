b8333c4e191d2887fa62b10f3719db81697be48e
Andrea Zonca <code@andreazonca.com>
2019-01-28 13:30:02 -0800

    run all LAT SAT channels

--- 201901_gaussian_fg_lensed_cmb_realistic_noise/run.py
+++ 201901_gaussian_fg_lensed_cmb_realistic_noise/run.py
@@ -11,9 +11,9 @@ import so_pysm_models
 simulator = mapsims.from_config(
     "example_config.cfg"
 )
-output_map = simulator.execute(write_outputs=False)[simulator.channels[0]]
+simulator.execute(write_outputs=True)
 
-expected_map = hp.read_map(
-    data.get_pkg_data_filename("tests/data/example_run.fits.gz", package="mapsims"), (0, 1, 2)
-)
-np.testing.assert_allclose(output_map, expected_map)
+#expected_map = hp.read_map(
+#    data.get_pkg_data_filename("tests/data/example_run.fits.gz", package="mapsims"), (0, 1, 2)
+#)
+#np.testing.assert_allclose(output_map, expected_map)
