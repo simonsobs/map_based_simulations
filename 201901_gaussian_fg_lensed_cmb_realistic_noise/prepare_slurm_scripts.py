33d6433e353acd6a08a82fa6290a2d4e14c3e756
Andrea Zonca <code@andreazonca.com>
2019-02-20 18:15:24 -0800

    production scripts

--- 201901_gaussian_fg_lensed_cmb_realistic_noise/prepare_slurm_scripts.py
+++ 201901_gaussian_fg_lensed_cmb_realistic_noise/prepare_slurm_scripts.py
@@ -8,21 +8,20 @@ for nside in [4096, 512]:
     for simulation_type in ["dust", "synchrotron", "noise", "cmb"]:
         folder = Path(f"output/{nside}/{simulation_type}")
         folder.mkdir(exist_ok=True)
-        #hours = 2 if highres and simulation_type in foregrounds else 1
-        hours = 0
+        hours = 2 if highres and simulation_type in foregrounds else 1
         minutes = 30
-        last_sim_id = 9 if highres else 99
+        last_sim_id = 9 if highres else 109
         if (not highres) and (simulation_type in foregrounds):
-            last_sim_id = 0
-        last_sim_id = 0
+            last_sim_id = 10
         with open(folder / "submit.slurm", "w") as f:
+            print("writing", f.name)
             f.write(template.format(
                 simulation_type=simulation_type,
                 nside=nside,
                 hours=hours,
                 minutes=minutes,
-                #channels="all" if highres else "SA",
-                channels="'SA_27,LA_27'",
-                first_sim_id=0,
+                channels="all" if highres else "SA",
+                #channels="'SA_27,LA_27'",
+                first_sim_id=0 if highres else 10,
                 last_sim_id=last_sim_id
             ))
